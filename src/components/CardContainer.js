0d5019d6290d7fcbdf02d1a9efbc8ee4479c9750
Patrick Owens <manbearpat@gmail.com>
2019-07-27 11:56:33 -0700

    minor changes

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,1 +1,21 @@
 import React, { Component } from "react";
+import Card from "./Card";
+
+class CardContainer extends Component {
+
+};
+
+handleCardClick = Card =>{
+    this.setState({ image: show})
+};
+
+return (
+    <div>
+        
+    </div>
+)
+
+
+
+
+export default CardContainer;
