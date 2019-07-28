e5e64d063415ea44b9201e5303625cfefc8e9695
Patrick Owens <manbearpat@gmail.com>
2019-07-27 22:55:10 -0700

    corrected typo

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -8,7 +8,7 @@ function Card(props) {
     return (
         <div>
             <div className="card">
-                <img src={props.image} class="card-img-top" alt={props.name} clicked={props.clicked}></img>
+                <img src={props.imageURL} class="card-img-top" alt={props.name} clicked={props.clicked}></img>
                 <div className="card-body">
                     <p className="card-text">{props.name}</p>
                 </div>
